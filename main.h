431a625118472a785e2f076548fa4948f0e60518
Armelle <armelledinde@yahoo.fr>
2022-05-18 23:44:28 +0200

    fix

--- main.h
+++ main.h
@@ -1,8 +1,8 @@
 #ifndef SHELL
 #define SHELL
 char *_getline(void);
-int shell(char ** argv, char **envp);
+int shell(char **argv, char **envp);
 char *_getline1();
-void _execve(char **, char **, char **);
+void _execve(char **arg, char **argv, char **envp);
 int shell1(void);
 #endif
